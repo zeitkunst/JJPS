91eb40e56bb0b8b82f5b69819fb974d414440ec4
zeitkunst <nknouf@zeitkunst.org>
2010-05-10 14:52:09 -0400

    -- nothing but a test of git

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@
 # The words "you", "licensee", and "recipient" are redefined to be as follows: "You", "licensee", and "recipient" is defined as anyone as long as s/he is not an EXCLUDED PERSON. An EXCLUDED PERSON is any individual, group, unit, component, synergistic amalgamation, cash-cow, chunk, CEO, CFO, worker, or organization of a corporation that is a member, as of the date of acquisition of this software, of the Fortune 1000 list of the world's largest businesses. (See http://money. cnn.com/magazines/fortune/global500/2008/full_list/ for an example of the top 500.) An EXCLUDED PERSON shall also include anyone working in a contractor, subcontractor, slave, or freelance capacity for any member of the Fortune 1000 list of the world's largest businesses.
 
 # Please see http://.org/license.
+# Just a test
 
 import sys
 
